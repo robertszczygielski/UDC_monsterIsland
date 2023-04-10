2f8ce2ed2d6bbae0f5fe3f8ea86a5d6b9abe19ac
Robert Szczygielski <robert.szczygielski@wp.pl>
2023-04-10 12:12:35 +0200

    Initial commit

---
+++ src/main/java/org/monster/monsterisland/MonsterislandApplication.java
@@ -0,0 +1,13 @@
+package org.monster.monsterisland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterislandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterislandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/monster/monsterisland/MonsterislandApplicationTests.java
@@ -0,0 +1,13 @@
+package org.monster.monsterisland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterislandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
